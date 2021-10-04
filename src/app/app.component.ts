70bad064ec9b4b1871f8d87bdd12b8abb2dc1440
Dextein Alberca <dextein.alberca.20@usjr.edu.ph>
2021-10-04 23:37:08 +0800

    Angular Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator-angular';
+
+  num1 = 0;
+  num2 = 0;
+  result = 0;
+
+  add(){
+    this.result = (this.num1 + this.num2);
+    let finalResult = document.getElementById("result") as HTMLInputElement;
+    finalResult.valueAsNumber = this.result;
+  }
+
+  minus(){
+    this.result = (this.num1 - this.num2);
+    let finalResult = document.getElementById("result") as HTMLInputElement;
+    finalResult.valueAsNumber = this.result;
+  }
+
+  multiply(){
+    this.result = (this.num1 * this.num2);
+    let finalResult = document.getElementById("result") as HTMLInputElement;
+    finalResult.valueAsNumber = this.result;
+  }
+
+  divide(){
+    this.result = (this.num1 / this.num2);
+    let finalResult = document.getElementById("result") as HTMLInputElement;
+    finalResult.valueAsNumber = this.result;
+  }
+
+  input1(value : any){
+    this.num1 = parseInt(value);
+  }
+
+  input2(value : any){
+    this.num2 = parseInt(value);
+  }
 }
